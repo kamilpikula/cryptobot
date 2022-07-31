f426fcbc477aac134d0bb72a82b607e6595c4ab4
kamilpikula <k.pikula97@gmail.com>
2022-07-31 16:35:45 +0200

    new app naive with scenarios

---
+++ naive/lib/naive/state.ex
@@ -0,0 +1,10 @@
+defmodule Naive.State do
+  @enforce_keys [:symbol, :profit_interval, :tick_size]
+  defstruct [
+    :symbol,
+    :buy_order,
+    :sell_order,
+    :profit_interval,
+    :tick_size
+  ]
+end
